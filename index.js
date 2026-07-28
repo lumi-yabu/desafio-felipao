4a662ffab5b645f0553356310378d89bee326c91
lumi-yabu <gabrielayabu@gmail.com>
2026-07-28 17:12:26 -0300

    adaptado com laços de repetição

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ let heroi = "Homem-Aranha"
 let xp = 9999
 let nivel
 
-if (xp <= 1000) {
-    nivel = "Ferro"
-} else if (xp <= 2000) {
-    nivel = "Bronze"
-} else if (xp <= 5000) {
-    nivel = "Prata"
-} else if (xp <= 7000) {
-    nivel = "Ouro"
-} else if (xp <= 8000) {
-    nivel = "Platina"
-} else if (xp <= 9000) {
-    nivel = "Ascendente"
-} else if (xp <= 10000) {
-    nivel = "Imortal"
-} else {
+const faixas = [
+    { limite: 1000, nome: "Ferro" },
+    { limite: 2000, nome: "Bronze" },
+    { limite: 5000, nome: "Prata" },
+    { limite: 7000, nome: "Ouro" },
+    { limite: 8000, nome: "Platina" },
+    { limite: 9000, nome: "Ascendente" },
+    { limite: 10000, nome: "Imortal" }
+]
+
+let i = 0
+while (i < faixas.length) {
+    if (xp <= faixas[i].limite) {
+        nivel = faixas[i].nome
+        break
+    }
+    i++
+}
+
+if (nivel === undefined) {
     nivel = "Radiante"
 }
 
